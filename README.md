d50d6209609254b3cfac00611ad200eaad960748
Sa3doun13 <mohammed.el-kholany@aau.at>
2022-10-31 10:56:54 +0100

    multi-objective optimization

--- README.md
+++ README.md
@@ -1,7 +1,41 @@
-# Semiconductor_scheduling_multi-shot
+# FJSP-SMT2020-multishot
 
-It contains an encoding of solving Job-shop Scheduling Problem, python api and instances \
+This work investigates solving semiconductor manufacturing scheduling tasks using Answer Set Programming (ASP). We tested our work (in progress) with small examples derived from the SMT2020 dataset.
 
-- The clingo package(5.6.1):
-    - [using optimization call] ``clingo-dl encoding_msw.lp instances\instance01..11.lp --minimize-variable=makespan``
-    - [using multi-shot] ``python dlO.py encoding_msw_multi-shot.lp instances\instance01..11.lp``
+- The following features have been considered in this work:
+    - Machine Maintenance
+    - Machine Setup
+    - Machine Assignment (Flexible/Fixed)
+- Our main objectives are to minimize the total completion time (makespan) of schedules and the machine-setup changes.
+
+This repository includes the following files/folders:
+
+<table>
+<tr><th>File/Folder</th><th>Description</th></tr>
+<tr><td style="font-family:'Courier New'">README.md</td><td>this file</td></tr>
+<tr><td style="font-family:'Courier New'">encoding_msw_multi-shot.lp</td><td>main scheduling encoding</td></tr>
+<tr><td style="font-family:'Courier New'">parsing.lp</td><td>auxiliary rules to reformat facts (included by <span style="font-family:'Courier New'">encoding_msw_multi-shot.lp</span>)</td></tr>
+<tr><td style="font-family:'Courier New'">machine_selection.lp</td><td>auxiliary rules to analyze machine groups (included by <span style="font-family:'Courier New'">encoding_msw_multi-shot.lp</span>)</td></tr>
+<tr><td style="font-family:'Courier New'">instances</td><td>instance files for testing</td></tr>
+</table>
+
+## Usage
+
+Our scheduling encoding and instances can be run with [Clingo[DL]](https://potassco.org/labs/clingodl/) as illustrated by the following example calls.
+
+We have two python versions for different packages:
+
+- Package[5.5.2]
+	- Fixed machine assignment:
+        - ``python dlO_multi-obj.py encoding_msw_multi-shot.lp instances/instance04.lp``
+
+	- Flexible machine assignment:
+        - ``python dlO_multi-obj.py encoding_msw_multi-shot.lp instances/instance04.lp --const flex=1``
+
+- Package[5.5.0]
+	- Fixed machine assignment:
+        - ``python dlO_5.5.0_multi-obj.py encoding_msw_multi-shot.lp instances/instance04.lp``
+
+	- Flexible machine assignment:
+        - ``python dlO_5.5.0_multi-obj.py encoding_msw_multi-shot.lp instances/instance04.lp --const flex=1``
+
--- dlO_5.5.0_multi-obj.py
+++ dlO_5.5.0_multi-obj.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python
 import sys
 import clingo
-from clingo import ast
-from clingo import Function, Number
-from clingodl import ClingoDLTheory
+import theory
 import time
-
+from clingo import Function
 
 NUM_OF_TIME_WINDOWS = 0
-MAX_TIMEOUT = 1000
+MAX_TIMEOUT = 600
+MAX_TIMEOUT_WEAK = 300
 
-
-class Application(clingo.Application):
-    '''
-    Application class similar to clingo-dl (excluding optimization).
-    '''
+class Application:
     def __init__(self, name):
-        self.__theory = ClingoDLTheory()
         self.program_name = name
-        self.version = ".".join(str(x) for x in self.__theory.version())
+        self.version = '1.0'
+        self.__theory = theory.Theory('clingodl', 'clingo-dl')
+        self.compressed_start_time = ''
+
+    def __on_model(self, model):
+        self.__theory.on_model(model)
 
     def register_options(self, options):
         self.__theory.register_options(options)
@@ -29,17 +28,11 @@ class Application(clingo.Application):
 
     def __on_statistics(self, step, accu):
         self.__theory.on_statistics(step, accu)
-
-    def __hidden(self, symbol):
-        return symbol.type == clingo.SymbolType.Function and symbol.name.startswith("__")
-    
-    #def __on_model(self, model):
-    #    self.__theory.on_model(model)
-
+        pass
 
     # get the Time out per Time Window
     def get_TimeOut(self):
-        return MAX_TIMEOUT/(NUM_OF_TIME_WINDOWS + 1)
+        return MAX_TIMEOUT/(NUM_OF_TIME_WINDOWS + 1), MAX_TIMEOUT_WEAK
     # ************************************************
 
     # get the assignment of the operations in a string format to be sent as facts for the next Time Window
@@ -58,12 +51,10 @@ class Application(clingo.Application):
 
     # get the part that should be grounded and solved
     def step_to_ground(self, prg, step):
-        #print('Start Time : {} '.format(step), start_time)
-
         string = 'solutionTimeWindow' + str(step)
         parts = []
         if step > 0:
-            parts.append(('subproblem', [Number(step)]))
+            parts.append(('subproblem', [step]))
             if step > 1:
                 parts.append((string, []))
                 prg.add(string, [], self.compressed_start_time)
@@ -74,23 +65,52 @@ class Application(clingo.Application):
 
     # add a new constraint to get lower value of bound (Optimization Part)
     def add_new_constraint(self, prg, bound):
-        #bound = 325
         prg.cleanup()
-        prg.ground([('opt', [Number(bound-1)])])
-        prg.assign_external(Function('bound', [Number(bound-1)]), True)
+        prg.ground([('opt', [bound-1])])
+        prg.assign_external(Function('bound', [bound-1]), True)
 
-    # ********************************************************************
+    # ground weak constraint with a limitation of the makespan value
+    def weak_const(self, prg, bound):
+        prg.ground([('weak', [bound])])
+        prg.assign_external(Function('bound', [bound]), True)
 
-    def main(self, prg, files):
-        #self.__theory.configure('propagate', 'full,1')
-        self.__theory.register(prg)
-        with ast.ProgramBuilder(prg) as bld:
-            ast.parse_files(files, lambda stm: self.__theory.rewrite_ast(stm, bld.add))
-
-        time_out_for_window = self.get_TimeOut()
-        i, ret = 0, None
+    def setup_opt(self, prg, time_out_for_weak, i):
+        time_used = 0
+        bound = 0
         start_time = ''
+        with prg.solve(on_model=self.__on_model, on_statistics=self.__on_statistics, async_=True, yield_=True) as hnd:
+            while True:
+                tic = time.time()
+                if time_used >= time_out_for_weak:
+                    break
+                hnd.resume()
+                wait = hnd.wait(time_out_for_weak - time_used)
+                if not wait:
+                    break
+                m = hnd.model()
+                if m is not None:
+                    a = self.__theory.assignment(m.thread_id)
+                    start_time, bound = self.get_total_facts(a, i)
+                    
+                else:
+                    #print('Optimum')
+                    break
+                
+                toc = time.time()
+                time_used += (toc - tic)
+        return start_time, bound
+    
+    def main(self, prg, files):
+        self.__theory.configure('propagate', 'full,1')
+        self.__theory.register(prg)
+        if not files:
+            files.append('-')
+        for f in files:
+            prg.load(f)
+        time_out_for_window, time_out_for_weak = self.get_TimeOut()
+        i = 0
         lastbound = 0
+        start_time = ''
         interrupted_calls = 0
         non_interrupted_calls = 0
         makespan_time_window = []
@@ -102,47 +122,44 @@ class Application(clingo.Application):
             prg.ground(parts)
             self.__theory.prepare(prg)
             bound = 0
-            def on_model(model):
-                nonlocal bound, start_time
-                self.__theory.on_model(model)
-                #if i != 0:
-                a = self.__theory.assignment(model.thread_id)
-                start_time, bound = self.get_total_facts(a, i)
-
+            count = 0
             while True:
-                prg.assign_external(Function('bound', [Number(lastbound-1)]), False)
+                prg.assign_external(Function('bound', [lastbound-1]), False)
                 lastbound = bound
                 tic = time.time()
                 if time_used >= time_out_for_window:
                     interrupted_calls += 1
                     break
-                with prg.solve(on_model=on_model, on_statistics=self.__on_statistics, async_=True, yield_=True) as handle:
+                with prg.solve(on_model=self.__on_model, on_statistics=self.__on_statistics, async_=True, yield_=True) as handle:
+                    handle.resume()
                     wait = handle.wait(time_out_for_window - time_used)
                     if not wait:
                         interrupted_calls += 1
                         break
-                    #print(handle.unsatisfiable)
-                    for model in handle:
-                        #print("Solution Found")
-                        break
+                    model = handle.model()
+                    if model is not None:
+                        a = self.__theory.assignment(model.thread_id)
+                        start_time, bound = self.get_total_facts(a, i)
                         
                     else:
-                        #prg.unsatisfiable == True:
                         non_interrupted_calls += 1
                         # sys.stdout.write("Optimum Found\n")
-                        print("Optimum for time window {}".format(i))
                         break
                 toc = time.time()
                 time_used += (toc - tic)
                 self.add_new_constraint(prg, bound)
-                
-            
+
+            self.weak_const(prg, lastbound)
+            start_time, lastbound = self.setup_opt(prg, time_out_for_weak, i)
+            #else:
+            #    ret = prg.solve()
             if i != 0:
                 makespan_time_window.append(lastbound)
             i = i + 1      # Go to the next Time Window
         for x in range(NUM_OF_TIME_WINDOWS):
             print('Completion Time for Window {} : {} '.format(x+1, makespan_time_window[x]))
-        print('Number of Interrupted Calls : {} '.format(interrupted_calls))
-        print('Number of UnInterrupted Calls : {} '.format(non_interrupted_calls))
+        print('Interr Calls : {} '.format(interrupted_calls))
+        print('UnInterr Calls : {} '.format(non_interrupted_calls))
+        print('Makespan : {} '.format(lastbound))
 
 sys.exit(int(clingo.clingo_main(Application('test'), sys.argv[1:])))
